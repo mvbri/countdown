c60dd6a30d1818f9f4f0b6a23ad06bb1e3cbcc40
maria <mvbri10@gmail.com>
2022-12-29 11:22:52 -0400

    Diagramado basico de countdown con promocion

---
+++ data.js
@@ -0,0 +1,24 @@
+export let monts = [
+	'January',
+	'february',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
+
+export let weekday = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesdat',
+	'Thursday',
+	'Friday',
+	'Saturday',
+];
